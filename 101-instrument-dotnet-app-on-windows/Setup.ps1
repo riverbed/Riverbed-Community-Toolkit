cdac0d93caab9330ff3ba1e1a3296329b492bf1e
Gwen Blum <19484478+gwenblum@users.noreply.github.com>
2021-11-19 20:08:27 +0100

    Update Setup.ps1

--- 101-instrument-dotnet-app-on-windows/Setup.ps1
+++ 101-instrument-dotnet-app-on-windows/Setup.ps1
@@ -2,10 +2,11 @@
 <#
     101-instrument-dotnet-app-on-windows
 
-    Step by step setup
-    - Tested on Windows Server 2019.
-    - Must run with Administrator privilege
-    - Need customization and replace {{my-CustomerId}}, {{my-SaaSAnalysisServerHost}} and optionaly {{my-S3-BUCKET}}
+   Step by step setup
+    - Must be customized, typically replacing {{my-CustomerId}}, {{my-SaaSAnalysisServerHost}} and optionaly {{my-S3-BUCKET}}
+    - Requires Administrator privileges
+   
+   Note: Tested on Windows Server 2019 instance in AWS
 
 #>
 
@@ -25,25 +26,25 @@ Start-BitsTransfer "$DOTNET_SDK_URL" -Destination C:\Packages\dotnet_sdk.exe
 C:\Packages\dotnet_sdk.exe /install /quiet /log C:\Logs\dotnet_sdk.log
 
 
-# Step 3. App
+# Step 3. Deploy a dotnet App
 
 dotnet new webapp -n YourApp
 Set-Location C:\YourApp
 dotnet run
 
-# at that point the application should be running on https://localhost:5001/ and you can connect to it from the local browser
+## at that point the application should be running on https://localhost:5001/ and you can connect to it from the local browser
 
 
-## Step 4. Aternity AppInternals Agent - DEM
+# Step 4. Aternity AppInternals Agent - DEM
 
-# Customize
+## Customize
 $O_SI_CUSTOMER_ID="{{my-CustomerId}}"  # e.g. 12341234-12341234-13241234
 $O_SI_SAAS_ANALYSIS_SERVER_HOST="{{my-SaaSAnalysisServerHost}}"  # e.g.  agents.apm.myaccount.aternity.com
 
-# Copy installer into  C:\Packages\Aternity-AppInternals_Agent.exe
+## Copy installer into  C:\Packages\Aternity-AppInternals_Agent.exe
 $ATERNITY_AGENT_URL = "https://{{my-S3-BUCKET}}/AppInternals_Agent_12.1.0.597_Win.exe"  # e.g.  "mydemobucket.s3.eu-central-1.amazonaws.com/AppInternals_Agent_12.1.0.597_Win.exe"
 Start-BitsTransfer $ATERNITY_AGENT_URL -Destination C:\Packages\Aternity-AppInternals_Agent.exe
 C:\Packages\Aternity-AppInternals_Agent.exe /s /v"O_SI_CUSTOMER_ID=$O_SI_CUSTOMER_ID O_SI_SAAS_ANALYSIS_SERVER_HOST=$O_SI_SAAS_ANALYSIS_SERVER_HOST O_SI_SAAS_ANALYSIS_SERVER_ENABLED=true O_SI_AUTO_INSTRUMENT=true /L*v c:\Logs\Aternity-Agent-InstallLog.log /qn"
 
-# at that point the Aternity APM agent should be running and can be tested with the following:
+## at that point the Aternity APM agent should be running and can be tested with the following:
 if (! (Get-Service AgentController).Status -eq 'Running') { Throw "Aternity Agent not running" }
