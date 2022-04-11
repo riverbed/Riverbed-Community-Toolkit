8f2c6f23f558539025e05e610f20e7ba92739e23
Gwen Blum <19484478+gwenblum@users.noreply.github.com>
2022-04-11 18:46:15 +0200

    110 update
    Fix traffic generator / zipkin

--- 110-opentelemetry-php-app/src/SampleZipkinExporter.php
+++ 110-opentelemetry-php-app/src/SampleZipkinExporter.php
@@ -12,6 +12,10 @@
 #   * OTEL_SERVICE_NAME set with service name for example, "service110-php"
 #   * OTEL_EXPORTER_ZIPKIN_ENDPOINT set with zipkin endpoint for example http://aternity-opentelemetry-collector:9411/api/v2/spans
 
+############################################
+
+## OpenTelemetry initialization
+
 declare(strict_types=1);
 require __DIR__ . '/vendor/autoload.php';
 
@@ -21,10 +25,6 @@ use OpenTelemetry\Contrib\Zipkin\Exporter as ZipkinExporter;
 use OpenTelemetry\SDK\Common\Attribute\Attributes;
 use OpenTelemetry\SDK\Trace\SpanProcessor\SimpleSpanProcessor;
 use OpenTelemetry\SDK\Trace\TracerProvider;
-
-############################################
-
-## OpenTelemetry initialization
 
 $OTEL_EXPORTER_ZIPKIN_ENDPOINT = getenv("OTEL_EXPORTER_ZIPKIN_ENDPOINT");
 $OTEL_SERVICE_NAME = getenv("OTEL_SERVICE_NAME");
@@ -59,13 +59,13 @@ $span->setAttribute('cookbook', '110')
 
 echo PHP_EOL ;
 
-for ($i = 1; $i < 5; $i++) {
+for ($i = 1; $i < 10; $i++) {
     
     echo "Span: $i" . PHP_EOL ;
 
     $span = $tracer->spanBuilder('loop-' . $i)->startSpan();
 
-    sleep(random_int(2,4));
+    sleep(random_int(1,4));
 
     $span->end();
 }
