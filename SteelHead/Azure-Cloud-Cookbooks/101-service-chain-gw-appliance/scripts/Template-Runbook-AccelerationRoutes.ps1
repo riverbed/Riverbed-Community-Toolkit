602463c3d6cb9a3bffe9bf863f707df38261d1c8
nil-cool <112728338+nil-cool@users.noreply.github.com>
2023-05-31 18:55:29 -0400

    Update Template-Runbook-AccelerationRoutes.ps1

--- SteelHead/Azure-Cloud-Cookbooks/101-service-chain-gw-appliance/scripts/Template-Runbook-AccelerationRoutes.ps1
+++ SteelHead/Azure-Cloud-Cookbooks/101-service-chain-gw-appliance/scripts/Template-Runbook-AccelerationRoutes.ps1
@@ -96,20 +96,6 @@ catch {
     throw $_.Exception
 }
 
-#Get all Resource Manager resources from all resource groups
-$ResourceGroups = Get-AzResourceGroup
-
-foreach ($ResourceGroup in $ResourceGroups)
-{    
-    Write-Output ("Showing resources in resource group " + $ResourceGroup.ResourceGroupName)
-    $Resources = Get-AzResource -ResourceGroupName $ResourceGroup.ResourceGroupName
-    foreach ($Resource in $Resources)
-    {
-        Write-Output ($Resource.Name + " of type " +  $Resource.ResourceType)
-    }
-    Write-Output ("")
-}
-
 #endregion
 
 
