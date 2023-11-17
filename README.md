df0996213fb62899fea754cd8a1f01a0b0149fe9
cmc-rvbd <148554203+cmc-rvbd@users.noreply.github.com>
2023-11-17 13:56:07 +0100

    Updated documentation, local version of the Cisco aci-endpoint-tracker script, modified YAML file
    Local version of the Cisco aci-endpoint-tracker script from the acitoolkit which fixes a bug and adds a single-pass option.
    Modified the YAML file so that the container image name matches that used in the command line example.
    Updated documentation to reflect the changes and elaborate a few points.

--- README.md
+++ README.md
@@ -1,31 +1,154 @@
-# Riverbed Community Toolkit
+## NetProfiler Cisco ACI integration - mapping ACI EPGs to NetProfiler Host Groups
+The following cookbook contains a description of a workflow on how to add **NetProfiler** Host Groups based on the endpoint groups connected to **Cisco ACI**.
 
-A public toolkit shared by [Riverbed](https://www.riverbed.com) and the [Riverbed Community](https://community.riverbed.com) for Solution Engineering and product integration
+## Workflow overview
 
-| Folders | Contents | Related 3rd party technology |
-| --- | --- | --- |
-| [Cloud Accelerator (SteelHead)](/SteelHead) | Community Cloud Cookbooks: scripts and howto to deploy the Riverbed Accelerator in the Cloud and in your labs | Azure, ARM, PowerShell, AWS, GNS3 |
-| [Client Accelerator](/Client%20Accelerator) | Cookbooks to interact with the REST API, use Ansible with [SteelScript](https://github.com/riverbed/steelscript), deploy the controller appliance with template in the Cloud | Python, Azure, REST, Ansible, Terraform |
-| [Portal](/Portal/Azure-Cloud-Cookbooks/101-portal-simple-demo) | Cloud Cookbooks and *work in progress* Dashboards examples | Azure, ARM |
-| [NetIM](/NetIM) | Cloud Cookbooks and Community library to share extensions for NetIM: GMC metrics, custom mibs, Robots, Synthetic-Test | Azure API, ARM, PowerShell, Python |
-| [AppResponse](/AppResponse) | Cloud Cookbooks for Azure. Ansible playbook example based on [SteelScript Python Library](https://github.com/riverbed/steelscript)| Ansible, Python, REST API |
-| [NetProfiler](/NetProfiler) | Deploy the Netprofiler with Terraform template in the Azure Cloud. Ansible playbook example based on [SteelScript Python Library](https://github.com/riverbed/steelscript)| Ansible, Terraform |
-| [SteelConnect](/SteelConnect) | Scripts, howtos and templates to deploy SteelConnect headend in Azure | Azure, Terraform, PowerShell |
-| [SteelScript](#) | *work in progress* Examples and use-cases based on [SteelScript Python Library](https://github.com/riverbed/steelscript) | Python |
-| [Aternity](https://github.com/Aternity/Tech-Community) | The [Aternity Tech-Community](https://github.com/Aternity/Tech-Community) is sharing cookbooks and technical contents for [Aternity DEM](https://www.aternity.com/): EUEM, APM and User Journey Intelligence | OpenTelemetry, code instrumentation, API, Windows, Linux, PowerShell |
+<img src="images/workflows.png" width="35%"/>
 
+## Prerequisites
+1. A host with Docker installed, probably a Linux host, and sufficient access to run create and run Docker containers
+2. A NetProfiler with credentials available for a user able to create and modify Host Groups
+3. Access to a Cisco ACI APIC with suitable credentials
 
+## Workflow description
+The above workflow consists of the following parts:  
+1. Docker container which runs the [Cisco ACI toolkit](https://developer.cisco.com/codeexchange/github/repo/datacenter/acitoolkit). 
+2. Docker container which runs [MySQL](https://www.mysql.com) server.
+2. Docker container which runs [Ansible](https://www.ansible.com/) and SteelScript which is used to configure NetProfiler.
 
-## How to contribute?
+The Cisco ACI toolkit contains a script [aci_endpointtracker](https://acitoolkit.readthedocs.io/en/latest/endpointtracker.html) which extracts all the endpoints from an indicated Tenant and stores them into a MySQL table (endpoints) with the following structure:
+```
+MariaDB [endpointtracker]> desc endpoints;
++-----------+-----------+------+-----+---------------------+-------------------------------+
+| Field     | Type      | Null | Key | Default             | Extra                         |
++-----------+-----------+------+-----+---------------------+-------------------------------+
+| mac       | char(18)  | NO   |     | NULL                |                               |
+| ip        | char(16)  | YES  |     | NULL                |                               |
+| tenant    | char(100) | NO   |     | NULL                |                               |
+| app       | char(100) | NO   |     | NULL                |                               |
+| epg       | char(100) | NO   |     | NULL                |                               |
+| interface | char(100) | NO   |     | NULL                |                               |
+| timestart | timestamp | NO   |     | current_timestamp() | on update current_timestamp() |
+| timestop  | timestamp | YES  |     | NULL                |                               |
++-----------+-----------+------+-----+---------------------+-------------------------------+
+```
 
-Feel free to submit any code using Pull Request (hit the Fork button at the top right corner to get a copy of the repo in your own account, modify your own copy of the repo and then create a [pull request from here](https://github.com/riverbed/Riverbed-Community-Toolkit/pulls)). 
+## Setting up
 
-For problem or suggestions, simply create [Issues](https://github.com/riverbed/Riverbed-Community-Toolkit/issues).
+Before starting the [docker-compose](docker-compose-final.yml) process, the custom Ansible container needs to be built using the following [docker-file](dockerfile-ansible).
+```
+docker build -t m_ansible:aci -f dockerfile-ansible .
+```
+Verify that the container image is now existing:
+```
+docker images m_ansible:aci
+```
+Modify the docker-compose file environment variables APIC_URL, APIC_LOGIN and APIC_PASSWORD to the working environment values:
+```
+   environment:
+      - APIC_URL=https://myapic.url
+      - APIC_LOGIN=admin
+      - APIC_PASSWORD=mysecret_password
+```
 
-Thanks for sharing with the [Riverbed Community](https://community.riverbed.com)!
+Start the docker-compose process:
+```
+docker-compose -f docker-compose-final.yml up -d
+```
+Verify that all 3 containers are up and running:
+```
+docker ps -a                     
+CONTAINER ID   IMAGE                    COMMAND                  CREATED          STATUS          PORTS                 NAMES
+c44e76fe01c9   dockercisco/acitoolkit   "sleep infinity"         32 seconds ago   Up 31 seconds                         acitoolkit
+008a14bc3c98   m_ansible:aci            "sleep infinity"         32 seconds ago   Up 31 seconds                         ansible
+dc237249a07a   mysql:latest             "docker-entrypoint.s…"   32 seconds ago   Up 31 seconds   3306/tcp, 33060/tcp   mysql_db
+```
+Copy in an modified version of the [aci-endpoint-tracker](acitoolkit/applications/endpointtracker/aci-endpoint-tracker-rvbd.py) script into the acitoolkit container.
+This is a slightly modified version which fixes a couple of issues and adds a "off-off" option to force the script to execute one scan of ACI, export endpoint data into MySQL and then exit rather than run perpetually updating the database when the ACI system changes.
+```
+docker cp acitoolkit/applications/endpointtracker/aci-endpoint-tracker-rvbd.py acitoolkit:/opt/acitoolkit/applications/endpointtracker/aci-endpoint-tracker-rvbd.py
+```
+Connect to the acitoolkit container and install the [mysql-connector](https://pypi.org/project/mysql-connector-python/) package and a compatible version of the protobuf package for the database connection.
+Then execute the Python script that gets the endpoint information and writes it in to the MySQL database:
+```
+docker exec -ti acitoolkit /bin/bash
+apt-get update && apt-get install -y python-pip && pip install protobuf==3.18.0 && pip install mysql-connector-python==8.0.21
+cd applications/endpointtracker/
+python aci-endpoint-tracker-rvbd.py -o
+```
+Verify that the database exists and is populated:
+```
+docker exec -ti m_ansible /bin/bash
+mysql -u root -ppassword -h 172.18.0.3 endpointtracker
+select * from endpoints limit 10;
+exit
+exit
+```
+Modify the [create-hostgroups.yml](app/create-hostgroups.yml) file with the NetProfiler details for your environment:
+```
+  vars:
+    host: "NetProfiler ipv4 address"
+    access_code: "Oauth access code"
+    tenant: "myTenant"
+```
+Run the ansible-playbook within the ansible container:
+```
+docker exec -ti ansible /bin/bash
+ansible-playbook -vvv create-hostgroups.yml
+```
+## Production
+The ACI script can be run in one-off mode (the default) or in daemon mode. In one-off mode, the script pulls the endpoint data from the APIC and adds the entries matching the specified Tenant to the MySQL database.
+If an entry already exists (matching the MAC address) then it is updated to capure any changed details. Without one-off mode (default is now one-off mode) ithe application will keep running, listening for APIC events and updating the database accordingly.
+In daemon mode, the application puts itself into the background.
 
-## License
+For deployment, one could run the script chain from a cronjob or cronjobs, e.g. once an hour:
+1. Run the ACI script to update the MySQL database
+2. Run the Ansible script to update the Host Group definitions
 
-Copyright (c) 2021 Riverbed Technology, Inc.
+## Limitations
+1. The current model only handles one Tenant. One might extend the model to support multiple tenants with separate Host Group Types per Tenant, for instance.
 
-The scripts provided here are licensed under the terms and conditions of the MIT License accompanying the software ("License"). The scripts are distributed "AS IS" as set forth in the License. The script also include certain third party code. All such third party code is also distributed "AS IS" and is licensed by the respective copyright holders under the applicable terms and conditions (including, without limitation, warranty and liability disclaimers) identified in the license notices accompanying the software.
+## Troubleshooting
+1. Make sure the access information and credentials are correct for the APIC
+2. Make sure the access information and credentials are correct for the NetProfiler - note that the OAuth tokens expire so will need renewing at some point
+3. If there are Python errors make sure that nothing is installed that is not compatible with the old version of Python used by the ACI toolkit (Python version 2.7)
+
+## Reference
+The [aci-endpoint-tracker](acitoolkit/applications/endpointtracker/aci-endpoint-tracker-rvbd.py) script provides the following online help:
+```
+root@c5aa4f455f11:/opt/acitoolkit/applications/endpointtracker# python aci-endpoint-tracker-rvbd.py --help
+usage: aci-endpoint-tracker-rvbd.py [-h] [-u URL] [-l LOGIN] [-p PASSWORD]
+                                    [--cert-name CERT_NAME] [--key KEY]
+                                    [--snapshotfiles SNAPSHOTFILES [SNAPSHOTFILES ...]]
+                                    [-i MYSQLIP] [-a MYSQLLOGIN]
+                                    [-s MYSQLPASSWORD] [-d] [--kill]
+                                    [--restart] [-o]
+
+Application that logs on to the APIC and tracks all of the Endpoints in a
+MySQL database.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -u URL, --url URL     APIC URL e.g. http://1.2.3.4
+  -l LOGIN, --login LOGIN
+                        APIC login ID.
+  -p PASSWORD, --password PASSWORD
+                        APIC login password.
+  --cert-name CERT_NAME
+                        X.509 certificate name attached to APIC AAA user
+  --key KEY             Private key matching given certificate, used to
+                        generate authentication signature
+  --snapshotfiles SNAPSHOTFILES [SNAPSHOTFILES ...]
+                        APIC configuration files
+  -i MYSQLIP, --mysqlip MYSQLIP
+                        MySQL IP address.
+  -a MYSQLLOGIN, --mysqllogin MYSQLLOGIN
+                        MySQL login ID.
+  -s MYSQLPASSWORD, --mysqlpassword MYSQLPASSWORD
+                        MySQL login password.
+  -d, --daemon          Run as a Daemon
+  --kill                if run as a process, kill it
+  --restart             if run as a process, restart it
+  -o, --oneoff          Run one pass only and exit
+  ```
+## Copyright (c) 2023 Riverbed Technology
---
+++ acitoolkit/applications/endpointtracker/aci-endpoint-tracker-rvbd.py
@@ -0,0 +1,297 @@
+#!/usr/bin/env python
+################################################################################
+#                                 _    ____ ___                                #
+#                                / \  / ___|_ _|                               #
+#                               / _ \| |    | |                                #
+#                  _____       / ___ \ |___ | |  _       _                     #
+#                 | ____|_ __ /_/_| \_\____|___|(_)_ __ | |_                   #
+#                 |  _| | '_ \ / _` | '_ \ / _ \| | '_ \| __|                  #
+#                 | |___| | | | (_| | |_) | (_) | | | | | |_                   #
+#                 |_____|_|_|_|\__,_| .__/ \___/|_|_| |_|\__|                  #
+#                     |_   _| __ __ |_|___| | _____ _ __                       #
+#                       | || '__/ _` |/ __| |/ / _ \ '__|                      #
+#                       | || | | (_| | (__|   <  __/ |                         #
+#                       |_||_|  \__,_|\___|_|\_\___|_|                         #
+#                                                                              #
+################################################################################
+#                                                                              #
+# Copyright (c) 2015 Cisco Systems                                             #
+# All Rights Reserved.                                                         #
+#                                                                              #
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may   #
+#    not use this file except in compliance with the License. You may obtain   #
+#    a copy of the License at                                                  #
+#                                                                              #
+#         http://www.apache.org/licenses/LICENSE-2.0                           #
+#                                                                              #
+#    Unless required by applicable law or agreed to in writing, software       #
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT #
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the  #
+#    License for the specific language governing permissions and limitations   #
+#    under the License.                                                        #
+#                                                                              #
+################################################################################
+"""
+Simple application that logs on to the APIC and displays all
+of the Endpoints.
+
+Slightly modified by Riverbed to fix a couple of issues and add a one-off (single pass) option
+"""
+import sys
+import acitoolkit.acitoolkit as aci
+import warnings
+import re
+import os
+import logging
+import time
+from daemon import Daemon
+
+try:
+    import mysql.connector as mysql
+except ImportError:
+    import pymysql as mysql
+
+
+def touch(fname, times=None):
+    """ Touch file """
+    with open(fname, 'a'):
+        os.utime(fname, times)
+
+
+def convert_timestamp_to_mysql(timestamp):
+    """
+    Convert timestamp to correct format for MySQL
+
+    :param timestamp: string containing timestamp in APIC format
+    :return: string containing timestamp in MySQL format
+    """
+    (resp_ts, remaining) = timestamp.split('T')
+    resp_ts += ' '
+    resp_ts = resp_ts + remaining.split('+')[0].split('.')[0]
+    return resp_ts
+
+
+def connect_mysql(args):
+    """
+    Connect to the MySQL database
+    :param args: command line arguments
+    :return: tuple of c, cnx
+    """
+    # Create the MySQL database
+    cnx = mysql.connect(user=args.mysqllogin,
+                        password=args.mysqlpassword,
+                        host=args.mysqlip)
+    if args.daemon:
+        logging.info("Connecting to mysql database")
+    c = cnx.cursor()
+
+    with warnings.catch_warnings():
+        warnings.filterwarnings('ignore')
+        c.execute('CREATE DATABASE IF NOT EXISTS endpointtracker;')
+        cnx.commit()
+    c.execute('USE endpointtracker;')
+    with warnings.catch_warnings():
+        warnings.filterwarnings('ignore')
+        c.execute('''CREATE TABLE IF NOT EXISTS endpoints (
+                         mac       CHAR(18) NOT NULL,
+                         ip        CHAR(16),
+                         tenant    CHAR(100) NOT NULL,
+                         app       CHAR(100) NOT NULL,
+                         epg       CHAR(100) NOT NULL,
+                         interface CHAR(100) NOT NULL,
+                         timestart TIMESTAMP NOT NULL,
+                         timestop  TIMESTAMP NULL);''')
+        cnx.commit()
+
+    return c, cnx
+
+
+def tracker(args):
+    """
+    Main Endpoint tracker
+    :param args: command line arguments
+    :return: None
+    """
+    # Login to APIC
+    session = aci.Session(args.url, args.login, args.password)
+    resp = session.login()
+    if not resp.ok:
+        print '%% Could not login to APIC'
+        sys.exit(0)
+
+    c, cnx = connect_mysql(args)
+
+    # Download all of the Endpoints and store in the database
+    endpoints = aci.Endpoint.get(session)
+    for ep in endpoints:
+        try:
+            epg = ep.get_parent()
+        except AttributeError:
+            continue
+        app_profile = epg.get_parent()
+        tenant = app_profile.get_parent()
+        if ep.if_dn:
+            for dn in ep.if_dn:
+                match = re.match('protpaths-(\d+)-(\d+)', dn.split('/')[2])
+                if match:
+                    if match.group(1) and match.group(2):
+                        int_name = "Nodes: " + match.group(1) + "-" + match.group(2) + " " + ep.if_name
+        else:
+            int_name = ep.if_name
+
+        try:
+            data = (ep.mac, ep.ip, tenant.name, app_profile.name, epg.name,
+                    ep.if_name, convert_timestamp_to_mysql(ep.timestamp))
+        except ValueError, e:
+            if args.daemon:
+                logging.info(e)
+            continue
+
+        c.execute("""SELECT * FROM endpoints
+                    WHERE mac="%s" AND
+                    timestop is null;""" % ep.mac)
+        
+        # If the list is not empty then we have matches - endpoints with the same MAC
+        ep_exists = len(c.fetchall())
+        if not ep_exists:
+            # Create a new entry
+            c.execute("""INSERT INTO endpoints (mac, ip, tenant,
+                         app, epg, interface, timestart)
+                         VALUES ('%s', '%s', '%s', '%s',
+                         '%s', '%s', '%s')""" % data)
+            cnx.commit()
+        else:
+            # This MAC address was already seen and something may have changed
+            # but perhaps nothing changed... we update anyway
+            updata = (ep.mac, ep.ip, tenant.name, app_profile.name, epg.name,
+                    ep.if_name, convert_timestamp_to_mysql(ep.timestamp), ep.mac)
+            c.execute("""UPDATE endpoints SET mac="%s", ip="%s", tenant="%s", app="%s", epg="%s", 
+                    interface="%s", timestart="%s" WHERE mac="%s" AND timestop is null""" % updata)
+
+    # Check if we are running in one-off mode and return if we are
+    if not args.oneoff:
+        return
+    
+    # Subscribe to live updates and update the database
+    sys.stdout.write("Starting subscribe to apic events")
+    aci.Endpoint.subscribe(session)
+    while True:
+        if aci.Endpoint.has_events(session):
+            ep = aci.Endpoint.get_event(session)
+            try:
+                epg = ep.get_parent()
+            except AttributeError:
+                continue
+            app_profile = epg.get_parent()
+            tenant = app_profile.get_parent()
+            if ep.is_deleted():
+                ep.if_name = None
+                data = (convert_timestamp_to_mysql(ep.timestamp),
+                        ep.mac,
+                        tenant.name)
+                update_cmd = """UPDATE endpoints SET timestop='%s',
+                                timestart=timestart
+                                WHERE mac='%s' AND tenant='%s' AND
+                                timestop is null""" % data
+                c.execute(update_cmd)
+            else:
+                if ep.if_dn:
+                    for dn in ep.if_dn:
+                        match = re.match('protpaths-(\d+)-(\d+)', dn.split('/')[2])
+                        if match:
+                            if match.group(1) and match.group(2):
+                                int_name = "Nodes: " + match.group(1) + "-" + match.group(2) + " " + ep.if_name
+                                pass
+                else:
+                    int_name = ep.if_name
+
+                data = (ep.mac, ep.ip, tenant.name, app_profile.name, epg.name,
+                        int_name, convert_timestamp_to_mysql(ep.timestamp))
+                insert_data = "'%s', '%s', '%s', '%s', '%s', '%s', '%s'" % data
+                query_data = ("mac='%s', ip='%s', tenant='%s', "
+                              "app='%s', epg='%s', interface='%s', "
+                              "timestart='%s'" % data).replace(',', ' AND')
+                select_cmd = """SELECT COUNT(*) FROM endpoints
+                                WHERE %s""" % query_data
+                c.execute(select_cmd)
+                for count in c:
+                    if not count[0]:
+                        insert_cmd = """INSERT INTO endpoints (mac, ip,
+                                        tenant, app, epg, interface,
+                                        timestart)
+                                        VALUES (%s)""" % insert_data
+                        c.execute(insert_cmd)
+            cnx.commit()
+        # Sleep or else the endpointtracker will take 100% cpu
+        time.sleep(0.1)
+
+
+class Daemonize(Daemon):
+    """
+    Daemonize the endpointtracker
+    Creates a daemon and then runs the tracker function
+    """
+    def __init__(self,
+                 args,
+                 pidfile,
+                 stdin='/var/log/endpointtracker.log',
+                 stdout='/var/log/endpointtracker.log',
+                 stderr='/var/log/endpointtracker.log'
+                 ):
+        self.args = args
+        if not os.path.isfile(stdout):
+            touch(stdout)
+
+        super(Daemonize, self).__init__(pidfile, stdin, stdout, stderr)
+
+    def run(self):
+        """If --daemon is set we run the tracker function
+        """
+        logging.basicConfig(filename='/var/log/endpointtracker.log',
+                            level=logging.INFO,
+                            format=('%(asctime)s %(message)s'))
+        logging.info('Starting endpointtracker')
+        while True:
+            try:
+                tracker(self.args)
+            except mysql.err.OperationalError:
+                logging.info("Lost connection to database, reconnecting in 10")
+                time.sleep(10)
+
+
+def main():
+    """
+    Main Endpoint Tracker routine
+    :return: None
+    """
+    # Take login credentials from the command line if provided
+    # Otherwise, take them from your environment variables file ~/.profile
+    description = ('Application that logs on to the APIC and tracks'
+                   ' all of the Endpoints in a MySQL database.')
+    creds = aci.Credentials(qualifier=('apic', 'mysql', 'daemon'),
+                            description=description)
+    
+    # add an additional argument to support a single-pass mode
+    creds.add_argument('-o', '--oneoff', help='Run one pass only and exit', action='store_false')
+    
+    args = creds.get()
+
+    if args.daemon or args.kill or args.restart:
+        args.daemon = True
+        pid = '/var/run/endpointtracker.pid'
+        daemon = Daemonize(args, pid)
+
+    if args.kill:
+        daemon.stop()
+    elif args.restart:
+        daemon.restart()
+    elif args.daemon:
+        daemon.start()
+    else:
+        tracker(args)
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
